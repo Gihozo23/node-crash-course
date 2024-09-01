d47c39dba7203c27ab4a848aad0e0d7441d44003
Gihozo23 <christellegihozo23@gmail.com>
2024-09-01 21:55:26 +0200

    feat: add new express app file

---
+++ appForNewViews.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+//register view engine
+app.set("view engine", "ejs");
+app.set("views", "newViews");
+
+app.listen(3000);
+app.get("/", (request, response) => response.render("index"));
+app.get("/about", (request, response) => {
+  response.render("about");
+});
+app.get("/about-us", (request, response) => {
+  response.redirect("about");
+});
+app.get("/blogs/create", (request, response) => response.render("create");)
+app.use((request, response) => {
+  response.status(404).render("404");
+});
