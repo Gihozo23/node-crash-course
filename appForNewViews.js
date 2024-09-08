416115750059ee57a235275dda3421b7d94a6688
Gihozo23 <christellegihozo23@gmail.com>
2024-09-08 20:27:09 +0200

    refactor: add middlewares

--- appForNewViews.js
+++ appForNewViews.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://gihozo:test1234567@nodecrashcourse.8tinf.mongodb.net/";
 //register view engine
 app.set("view engine", "ejs");
 app.set("views", "newViews");
 
 app.listen(3000);
-app.get("/", (request, response) => response.render("index"));
+const blogs = [];
+app.get("/", (request, response) => response.render("index", { blogs }));
 app.get("/about", (request, response) => {
   response.render("about");
 });
+app.use(express.static("assets"));
+app.use(morgan("tiny"));
 app.get("/about-us", (request, response) => {
   response.redirect("about");
 });
